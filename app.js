cba65ea7dbb223909781c300142415ca10a40093
isaev_md <isaev_md@astral.ru>
2020-10-31 19:03:20 +0300

    create client folder

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log(111);
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const router = require("./routes/auth.routes");
+
+const app = express();
+
+const PORT = config.get("port") || 5000;
+const MONGO_URI = config.get("mongoUri");
+
+app.use("/api/auth", router);
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    //Метод connect позоволяет подключиться к базе даннных mongoDB
+
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(`Error ${error.message}`);
+    process.exit(1);
+    // Выход из процесса node.js
+  }
+}
+
+start();
